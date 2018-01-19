b3dc2c63f7541fa17f2b72493f12dbb6d15ccb3d
futurist <jamesyang999@gmail.com>
2018-01-19 18:29:16 +0800

    add regions

--- index.js
+++ index.js
@@ -28,14 +28,14 @@ var defaultOptions = {
   },
 }
 function getParts(i, j, lines, cols){
-  let V = i==0 ? 'T' : i>=lines-1 ? 'B' : 'C'
-  let H = j==0 ? 'L' : j>=cols-1 ? 'R' : 'C'
+  let V = i>=lines-1 ? 'B' : i==0 ? 'T' : 'C'
+  let H = j>=cols-1 ? 'R' : j==0 ? 'L' : 'C'
   return [H, V]
 }
 function gridLines(lines, cols, opt){
   const ret = []
   opt = objutil.merge({}, defaultOptions, opt)
-  const {style} = opt
+  const {style, regions} = opt
   style.CB=style.CB||style.LB
   style.RC=style.RC||style.RT
   for(let i=0;i<lines; i++){
@@ -43,13 +43,19 @@ function gridLines(lines, cols, opt){
     for(let j=0;j<cols; j++){
       let [H, V] = getParts(i, j, lines, cols)
       let css = objutil.assign({}, style[H+V] || style.LT)
-      let rh=2, rw=1
-      if(i<rh&&j<rw) {
-        let [H2, V2] = getParts(i,j,rh, rw)
-        console.log(i,j,H2,V2)
-        // corner case, i==0, lines==1, T will B!!
-        if(i<rh-1 && V2!='B') css.borderBottom = 0
-        if(j<rw-1 && H2!='R') css.borderRight=0
+      if(Array.isArray(regions)){
+        regions.forEach(arr=>{
+          let [x,y,w,h] = arr
+          let rh = y+h
+          let rw = x+w
+          if(i>=y && i<rh && j>=x && j<rw) {
+            let [H2, V2] = getParts(i,j,rh, rw)
+            // console.log(i,j,H2,V2)
+            // corner case, i==0, lines==1, T will B!!
+            if(V2!='B') css.borderBottom = 0
+            if(H2!='R') css.borderRight=0
+          }
+        })
       }
       ret.push('<td style="'+inlineStyle(css)+'">', i*cols+j, '</td>')
     }
@@ -58,6 +64,6 @@ function gridLines(lines, cols, opt){
   return ret.join(opt.newLine)
 }
 
-require('fs').writeFileSync('test.html', `<table cellpadding=0 cellspacing=0>${gridLines(5,3, {style:{LT:{border:'1px solid red'}}})}</table>`, 'utf8')
+require('fs').writeFileSync('test.html', `<table cellpadding=0 cellspacing=0>${gridLines(8,9, {regions:[[1,2,1,2],[3,4,2,2]], style:{LT:{border:'1px solid red'}}})}</table>`, 'utf8')
 
 
