c49c49ae0b687559b446a88ec9d4638cdea3a2f2
futurist <jamesyang999@gmail.com>
2018-01-19 19:29:39 +0800

    fix getParts bug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = gridLines
 
 const objutil = require('objutil')
+const {isArray} = Array
 
 function dashify (str) {
   return str.replace(/[A-Z]/g, function (m) {
@@ -27,9 +28,9 @@ var defaultOptions = {
     RB: {border:0}
   },
 }
-function getParts(i, j, lines, cols){
-  let V = i>=lines-1 ? 'B' : i==0 ? 'T' : 'C'
-  let H = j>=cols-1 ? 'R' : j==0 ? 'L' : 'C'
+function getParts(i, j, lines, cols, x, y){
+  let V = i>=lines-1 ? 'B' : i==(y|0) ? 'T' : 'C'
+  let H = j>=cols-1 ? 'R' : j==(x|0) ? 'L' : 'C'
   return [H, V]
 }
 function gridLines(lines, cols, opt){
@@ -43,17 +44,23 @@ function gridLines(lines, cols, opt){
     for(let j=0;j<cols; j++){
       let [H, V] = getParts(i, j, lines, cols)
       let css = objutil.assign({}, style[H+V] || style.LT)
-      if(Array.isArray(regions)){
-        regions.forEach(arr=>{
-          let [x,y,w,h] = arr
+      if(isArray(regions)){
+        regions.forEach(obj=>{
+          if(!obj) return
+          if(isArray(obj)){
+            var [x,y,w,h] = obj
+          }else{
+            var {x,y,w,h,style} = obj
+          }
           let rh = y+h
           let rw = x+w
           if(i>=y && i<rh && j>=x && j<rw) {
-            let [H2, V2] = getParts(i,j,rh, rw)
-            // console.log(i,j,H2,V2)
+            let [H2, V2] = getParts(i,j,rh, rw, x, y)
+            console.log(i,j,H2,V2)
             // corner case, i==0, lines==1, T will B!!
             if(V2!='B') css.borderBottom = 0
             if(H2!='R') css.borderRight=0
+            if(style) objutil.assign(css, style[H2+V2])
           }
         })
       }
@@ -64,6 +71,6 @@ function gridLines(lines, cols, opt){
   return ret.join(opt.newLine)
 }
 
-require('fs').writeFileSync('test.html', `<table cellpadding=0 cellspacing=0>${gridLines(8,9, {regions:[[1,2,1,2],[3,4,2,2]], style:{LT:{border:'1px solid red'}}})}</table>`, 'utf8')
+require('fs').writeFileSync('test.html', `<table cellpadding=0 cellspacing=0>${gridLines(8,9, {regions:[{x:1,y:2,w:1,h:2,style:{RB:{borderBottom:'3px solid black'}}},[3,4,2,2]], style:{LT:{border:'1px solid red'}}})}</table>`, 'utf8')
 
 
